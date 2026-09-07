89e5eb676c47ee0ad5f38fc59260172631d96490
GhostPunishR <68479390+GhostPunishR@users.noreply.github.com>
2026-09-07 12:48:09 +0200

    refactor(app): extraire les intents Discord

---
+++ src/app/intents.rs
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2026 FoxSecura contributors
+// SPDX-License-Identifier: AGPL-3.0-only
+
+use poise::serenity_prelude::GatewayIntents;
+
+pub fn default() -> GatewayIntents {
+    GatewayIntents::GUILDS
+        | GatewayIntents::GUILD_MODERATION
+        | GatewayIntents::GUILD_MEMBERS
+}
