a74f3ffa28affe30e7ea1d6a3fc0ffcb9dededf2
GhostPunishR <68479390+GhostPunishR@users.noreply.github.com>
2026-09-10 18:01:08 +0200

    refactor: keep binary entrypoint thin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 // SPDX-FileCopyrightText: 2026 FoxSecura contributors
 // SPDX-License-Identifier: AGPL-3.0-only
 
-mod app;
-mod commands;
+use foxsecura::app;
 
 #[tokio::main]
 async fn main() -> Result<(), app::Error> {
